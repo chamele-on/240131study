ebfd1f510a8aeb5cf69acce739f3b698a3cfcab3
chanwoo <chamele-on.skykitty13@naver.com>
2024-01-31 17:29:31 +0900

    [수업내용 정리] 2024-01-31

---
+++ src/main/java/com/ohgiraffers/section01/literal/Application01.java
@@ -0,0 +1,26 @@
+package com.ohgiraffers.section01.literal;
+
+import com.sun.tools.javac.Main;
+
+public class Application01 {
+    public static void main(String[] args) {
+        /*수업목표. 여러가지 값의 형태를 출력할 수 있다.*/
+
+        /*목차. 1. 숫자 형태의 값 */
+        System.out.println(123);
+
+        /*목차. 1-1. 실수 형태의 값 출력 */
+        System.out.println(1.23);
+        /*목차. 2. 실수 형태의 값 출력 */
+        System.out.println('a');
+        System.out.println('1');
+        /*목차. 3. 문자열 형태의 값 출력*/
+        System.out.println("안녕하세요 조평훈 입니다.");    // 문자열은 문자 여러 개가 나열 된 형태를 말하며 쌍따옴표(double-quotation)으로 감싸주어야 한다.
+        System.out.println("123");
+        System.out.println("");
+        System.out.println("a");
+        /*목차. 4. 논리 형태의 값 출력 */
+        System.out.println(true);
+        System.out.println(false);
+    }
+}
